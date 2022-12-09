ef5e25bcc8861420aa19201dd86aeb51e9c6972c
Danila <taradash98@gmail.com>
2022-12-09 18:02:18 +0300

    Task1 Complited

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+string n = Console.ReadLine();
+int x = n.Length;
+
+if (x == 5)
+{
+    if (n[0] == n[4] && n[1] == n[3])
+    {
+        Console.WriteLine($"{n} Полидром");
+    }
+    else 
+    {
+        Console.WriteLine($"{n} Не полидром");
+    }
+
+}
+else
+{
+    Console.WriteLine($"{n} Не пятизначное число ");
+}
